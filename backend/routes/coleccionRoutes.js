7541454a6b3c73d0ecef2ed5db716252350a85fd
Ailén Villalba <ailuv0324@gmail.com>
2026-06-21 19:54:42 -0300

    Funciones implementadas en coleccionController.js, y archivo coleccionRoutes.js modificado

--- backend/routes/coleccionRoutes.js
+++ backend/routes/coleccionRoutes.js
@@ -1,13 +1,17 @@
 const { Router } = require('express');
 const router = Router();
 const { middlewareVideojuegos } = require("../middleware/videojuegosValidator.middleware");
-const { getAllColecciones, getColeccionById, putEstado, putCalifacion, putTiempoJuego, deleteColeccion } = require('../controllers/coleccionController');
+const { getAllColecciones, getAllColeccionesUsuarios, getColeccionById, postColeccion, putColeccion } = require('../controllers/coleccionController');
 
 router.get('/', getAllColecciones);
-router.get('/:id', getColeccionesById);
+router.get('/:id', getAllColeccionesUsuario)
+router.get('/:id', getColeccionById);
+router.post('/', postColeccion);
+router.put('/', putColeccion);
+/*
 router.put('/', putEstado);
 router.put('/', putCalifacion);
 router.put('/', putTiempoJuego);
 router.delete('/', deleteColeccion);
-
+*/
 module.exports = router;
