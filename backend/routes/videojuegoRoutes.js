547a7b516827f47bcd425175b02ebcf560ef99e9
Andres Aguayo <andyaguayo49@gmail.com>
2026-06-21 02:59:53 -0300

    Middleware de validacion implementado en post de videojuegosRoutes

--- backend/routes/videojuegoRoutes.js
+++ backend/routes/videojuegoRoutes.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const router = express.Router();
+const { Router } = require('express');
+const router = Router();
+const { middlewareVideojuegos } = require("../middleware/videojuegosValidator.middleware");
 const videojuegoController = require('../controllers/videojuegoController');
 
-// Definir rutas
-// GET /api/videojuegos - Obtener todos los videojuegos
+// GET Obtener todos los videojuegos
 router.get('/', videojuegoController.getAllVideojuegos);
 
-// GET /api/videojuegos/:id - Obtener un videojuego por ID
+// GET Obtener un videojuego por ID
 router.get('/:id', videojuegoController.getVideojuegoById);
 
-// POST /api/videojuegos - Crear un nuevo videojuego
-router.post('/', videojuegoController.createVideojuego);
+// POST Crear un nuevo videojuego
+router.post('/', middlewareVideojuegos, videojuegoController.createVideojuego);
 
-// PUT /api/videojuegos/:id - Actualizar un videojuego
-router.put('/:id', videojuegoController.updateVideojuego);
+// PUT Actualizar un videojuego
+router.put('/:id', videojuegoController.updateVideojuego);      // <--- VER SI LA DEJAMOS O NO
 
 // DELETE /api/videojuegos/:id - Eliminar un videojuego
-router.delete('/:id', videojuegoController.deleteVideojuego);
+router.delete('/:id', videojuegoController.deleteVideojuego);   // <--- VER SI LA DEJAMOS O NO
 
 module.exports = router;
