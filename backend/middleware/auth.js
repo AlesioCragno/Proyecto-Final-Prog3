1485d0af61fc97d2944f2d8de4edea63188a959e
Andres Aguayo <andyaguayo49@gmail.com>
2026-06-21 23:16:24 -0300

    generarToken actualizado y corregido

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,10 +3,11 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET || 'secret_por_defecto';
 
 function generarToken(user) {
-  token = jwt.sign(
-    { id: user.id, email: user.email }, JWT_SECRET, { expiresIn: '24h' }
+  const token = jwt.sign(
+    { id: user.id, email: user.email },
+    JWT_SECRET,
+    { expiresIn: '24h' }
   );
-
   return token;
 }
 
