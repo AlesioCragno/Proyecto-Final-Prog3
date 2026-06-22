907b98e92f6c0e24fcba7eea46c5638aa8c6d739
Andres Aguayo <andyaguayo49@gmail.com>
2026-06-22 00:56:24 -0300

    serverjs corregido

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,12 @@ require("dotenv").config();
 const { sequelize } = require("./models/indexModel");
 const routes = require("./routes");
 
+const { establecerCardinalidad } = require("./models/cardinalidadesModel");
+
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT || 3001;
-    this.databaseConfig();
     this.middleware();
     this.routes();
   }
@@ -38,18 +39,9 @@ class Server {
   routes() {
     this.app.use("/api", routes);
 
-    this.app.get("/health", (req, res) => {
-      res.status(200).json({
-        status: "OK",
-        timestamp: new Date().toISOString(),
-        uptime: process.uptime(),
-      });
-    });
-
     this.app.use((err, req, res, next) => {
-      res.status(500).json({
-        error: err.message,
-      });
+      console.error("Error no controlado detectado:", err.stack);
+      res.status(err.status || 500).json({ error: err.message || "Internal Server Error"});
     });
 
     this.app.use("*", (req, res) => {
@@ -59,17 +51,13 @@ class Server {
     });
   }
 
-  async databaseConfig() {
-    try {
-      await sequelize.sync({ alter: false });
-    } catch (error) {
-      console.error("Error configuring database:", error);
-    }
-  }
-
   // Inicializar servidor
   async startServer() {
     try {
+      // Establecer cardinalidades
+      console.log("Configurando relaciones y cardinalidades.")
+      establecerCardinalidad();
+
       // Probar conexión a la base de datos
       await sequelize.authenticate();
       console.log("✅ Database connection established successfully.");
