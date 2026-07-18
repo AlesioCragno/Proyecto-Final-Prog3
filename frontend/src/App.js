b70ddd40520d6a9e6227adc3bae6ad5f28ed6383
Andres Aguayo <andyaguayo49@gmail.com>
2026-07-18 17:59:21 -0300

    Arreglos para testeo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,16 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 // Componentes comunes
-import { Navbar } from "./components/common/Navbar";
-import { Footer } from "./components/common/Footer";
+//import { Navbar } from "./components/common/Navbar";
+//import { Footer } from "./components/common/Footer";
 
 // Componentes de gestion
-import { GestionUsuarios } from "./components/GestionUsuarios";
-import { GestionVideojuegos} from "./components/GestionVideojuegos";
+//import { GestionUsuarios } from "./components/GestionUsuarios";
+//import { GestionVideojuegos} from "./components/GestionVideojuegos";
 import { GestionColecciones} from "./components/GestionColecciones";
 
 // JWT
-import { Login } from "./components/JWT/Login"
+//import { Login } from "./components/JWT/Login"
 import { Register } from "./components/JWT/Register";
 import { Perfil } from "./components/JWT/Perfil";
 import { ListaUsuarios } from "./components/JWT/ListaUsuarios";
@@ -21,20 +21,20 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar/>
+        {/*<Navbar/>*/}
 
         <main style={{ padding: "20px"}}>
           <h1>Panel de Control - Coleccion de Videojuegos</h1>
 
           <Routes>
             {/* Rutas del catalogo y colecciones */}
-            <Route path="/" element={<GestionVideojuegos/>}/>
-            <Route path="/videojuegos" element={<GestionVideojuegos/>}/>
+            {/*<Route path="/" element={<GestionVideojuegos/>}/>*/}
+            {/*<Route path="/videojuegos" element={<GestionVideojuegos/>}/>*/}
             <Route path="/colecciones" element={<GestionColecciones/>}/>
-            <Route path="/usuarios" element={<GestionUsuarios/>}/>
+            {/*<Route path="/usuarios" element={<GestionUsuarios/>}/>*/}
 
             {/* Rutas de JWT */}
-            <Route path="/login" element={<Login/>}/>
+            {/*<Route path="/login" element={<Login/>}/>*/}
             <Route path="/register" element={<Register/>}/>
             <Route path="/perfil" element={<Perfil/>}/>
             <Route path="/lista-usuarios" element={<ListaUsuarios/>}/>
@@ -42,7 +42,7 @@ function App() {
           </Routes>
         </main>
 
-        <Footer/>
+        {/*<Footer/>*/}
       </div>
     </Router> 
   );
