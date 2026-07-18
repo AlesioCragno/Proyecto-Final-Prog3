09c8d2d43853e260e696d3e94e5f39aeb7d84216
Andres Aguayo <andyaguayo49@gmail.com>
2026-07-18 15:14:58 -0300

    imports agregados temporales

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+// Componentes comunes
+import { Navbar } from "./components/common/Navbar";
+import { Footer } from "./components/common/Footer";
+
+// Componentes de gestion
+import { GestionUsuarios } from "./components/GestionUsuarios";
+import { GestionVideojuegos} from "./components/GestionVideojuegos";
+import { GestionColecciones} from "./components/GestionColecciones";
+
+// JWT
+import { Login } from "./components/JWT/Login"
+import { Register } from "./components/JWT/Register";
+import { Perfil } from "./components/JWT/Perfil";
+import { ListaUsuarios } from "./components/JWT/ListaUsuarios";
 
 function App() {
   return (
