2dbd08de79fa2cfdc39328cdcf1e7b00934a4d65
Andres Aguayo <andyaguayo49@gmail.com>
2026-07-18 17:26:16 -0300

    Cardinalidades actualizadas

--- backend/models/cardinalidadesModel.ts
+++ backend/models/cardinalidadesModel.ts
@@ -5,4 +5,7 @@ import { Coleccion } from "./coleccionModel";
 export const establecerCardinalidad = (): void => {
     User.belongsToMany(Videojuego, { through: Coleccion, foreignKey: "userId" });
     Videojuego.belongsToMany(User, { through: Coleccion, foreignKey: "videojuegoId"})
+
+    Coleccion.belongsTo(Videojuego, { foreignKey: "videojuegoId" });
+    Coleccion.belongsTo(User, { foreignKey: "userId" });
 }
